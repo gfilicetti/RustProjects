64baa02d38b7329dd80446d3e1a57a7a4b92db0a
Gino Filicetti <gino@filicetti.net>
2020-05-24 22:14:17 -0400

    adding code

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Url;
+
+#[tokio::main]
+async fn main() {
+    let arg = std::env::args()
+        .nth(1)
+        .expect("Expect a command line argument but didn't get one");
+    let url = Url::parse(&arg).expect("You didn't pass me a valid url");
+    let response = reqwest::get(url).await.expect("Couldn't reach the internet");
+    assert!(response.status() == 200, "Did not get a 200 OK");
+    println!("{}", response.text().await.expect("Could not read the response body"));
 }
